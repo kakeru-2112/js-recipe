5366448731af03327a4ade43ca345fc99d98dfa0
kakeru-2112 <kake320_tako8@yahoo.co.jp>
2021-01-15 18:35:15 +0900

    進みました

---
+++ 1_workspace/1/カウンター/calculator.js
@@ -0,0 +1,152 @@
+const display = document.getElementById("display")
+const clear = document.getElementById("clear-button")
+const division = document.getElementById("division-button")
+const seven = document.getElementById("seven-button")
+const eight = document.getElementById("eight-button")
+const nine = document.getElementById("nine-button")
+const multi = document.getElementById("multi-button")
+const four = document.getElementById("four-button")
+const five = document.getElementById("five-button")
+const six = document.getElementById("six-button")
+const minus = document.getElementById("minus-button")
+const one = document.getElementById("one-button")
+const two = document.getElementById("two-button")
+const three = document.getElementById("three-button")
+const plus = document.getElementById("plus-button")
+const zero = document.getElementById("zero-button")
+const equal = document.getElementById("equal-button")
+
+
+let display_num = ""
+let record_num = ""
+let culc_num = null
+let calc = "0"
+  
+one.onclick = function(){
+  display_num+="1" 
+  record_num+="1" 
+  display.textContent = display_num
+}
+two.onclick = function(){ 
+  display_num+="2" 
+  record_num+="2"
+  display.textContent = display_num
+}
+three.onclick = function(){  
+  display_num+="3"
+  record_num+="3"
+  display.textContent = display_num
+}
+four.onclick = function(){ 
+  display_num+="4"
+  record_num+="4"
+  display.textContent = display_num
+}
+five.onclick = function(){ 
+  display_num+="5" 
+  record_num+="5"
+  display.textContent = display_num
+}
+six.onclick = function(){
+  display_num+="6" 
+  record_num+="6"  
+  display.textContent = display_num
+}
+seven.onclick = function(){ 
+  display_num+="7" 
+  record_num+="7"
+  display.textContent = display_num
+}
+eight.onclick = function(){ 
+  display_num+="8"
+  record_num+="8" 
+  display.textContent = display_num
+}
+nine.onclick = function(){  
+  display_num+="9"
+  record_num+="9"
+  display.textContent = display_num
+}
+zero.onclick = function(){
+  display_num+="0"
+  record_num+="0"
+  display.textContent = display_num
+}
+
+plus.onclick = function(){
+  if (culc_num===null){
+    culc_num = Number(record_num)
+  }else {
+    culc_num += Number(record_num)
+  }
+  display_num+="+"
+  record_num = ""
+  calc = "1"
+  display.textContent = display_num
+
+}
+minus.onclick = function(){
+  if (culc_num===null){
+    culc_num = Number(record_num)
+  }else {
+    culc_num -= Number(record_num)
+  }
+  display_num+="-"
+  record_num = ""
+  calc = "2"
+  display.textContent = display_num 
+
+}
+multi.onclick = function(){
+  if (culc_num===null){
+    culc_num = Number(record_num)
+  }else {
+    culc_num *= Number(record_num)
+  }
+  display_num+="×"
+  record_num = ""
+  calc = "3"
+  display.textContent = display_num 
+
+}
+division.onclick = function(){
+  if (culc_num===null){
+    culc_num = Number(record_num)
+  }else {
+    culc_num /= Number(record_num)
+  }
+  display_num+="÷"
+  record_num = ""
+  calc = "4"
+  display.textContent = display_num 
+
+}
+
+clear.onclick = function(){
+  display_num =""
+  record_num = "" 
+  culc_num = null
+  calc = "0"
+  display.textContent = 0
+}
+
+equal.onclick = function(){ 
+  if (calc === "1"){
+    display.textContent = culc_num+Number(record_num)
+  }else if(calc === "2"){
+    display.textContent = culc_num-Number(record_num)
+  }else if(calc === "3"){
+    display.textContent = culc_num*Number(record_num)
+  }else if(calc === "4"){
+    display.textContent = culc_num/Number(record_num)
+  }
+
+  display_num =""
+  record_num = "" 
+  culc_num = null
+  calc = "0"
+  
+}
+
+
+
