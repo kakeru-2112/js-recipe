7447c66c6829807cfaab6fbc95d2c671c824ea5c
kakeru-2112 <kake320_tako8@yahoo.co.jp>
2021-01-16 21:58:19 +0900

    電卓アップデート

--- 1_workspace/1/カウンター/calculator.js
+++ 1_workspace/1/カウンター/calculator.js
@@ -1,5 +1,6 @@
 const display = document.getElementById("display")
 const clear = document.getElementById("clear-button")
+const plumin = document.getElementById("plumin-button")
 const division = document.getElementById("division-button")
 const seven = document.getElementById("seven-button")
 const eight = document.getElementById("eight-button")
@@ -18,13 +19,12 @@ const dot = document.getElementById("dot-button")
 const equal = document.getElementById("equal-button")
 
 
-let display_num = ""
 let record_num = ""
 let calc_num = null
 let calc = "0"
 
 const calcFunction = function(){
-  if (calc_num===null){
+  if (calc_num === null){
     calc_num = Number(record_num)
   }else {
     if (calc === "1"){
@@ -39,112 +39,158 @@ const calcFunction = function(){
   }
 }
   
-one.onclick = function(){
-  display_num+="1" 
-  record_num+="1" 
-  display.textContent = display_num
+one.onclick = function(){ 
+  record_num+="1"
+  display.textContent = record_num
 }
 two.onclick = function(){ 
-  display_num+="2" 
   record_num+="2"
-  display.textContent = display_num
+  display.textContent = record_num
 }
 three.onclick = function(){  
-  display_num+="3"
   record_num+="3"
-  display.textContent = display_num
+  display.textContent = record_num
 }
 four.onclick = function(){ 
-  display_num+="4"
   record_num+="4"
-  display.textContent = display_num
+  display.textContent = record_num
 }
-five.onclick = function(){ 
-  display_num+="5" 
+five.onclick = function(){  
   record_num+="5"
-  display.textContent = display_num
+  display.textContent = record_num
 }
-six.onclick = function(){
-  display_num+="6" 
+six.onclick = function(){ 
   record_num+="6"  
-  display.textContent = display_num
+  display.textContent = record_num
 }
 seven.onclick = function(){ 
-  display_num+="7" 
   record_num+="7"
-  display.textContent = display_num
+  display.textContent = record_num
 }
 eight.onclick = function(){ 
-  display_num+="8"
   record_num+="8" 
-  display.textContent = display_num
+  display.textContent = record_num
 }
 nine.onclick = function(){  
-  display_num+="9"
   record_num+="9"
-  display.textContent = display_num
+  display.textContent = record_num
 }
 zero.onclick = function(){
-  display_num+="0"
   record_num+="0"
-  display.textContent = display_num
+  display.textContent = record_numm
 }
 
 plus.onclick = function(){
+  if(minus.classList.contains("onclick-button") == true ){
+    minus.classList.remove("onclick-button")
+   } else if(multi.classList.contains("onclick-button") == true ){
+    multi.classList.remove("onclick-button")
+   } else if(division.classList.contains("onclick-button") == true ){
+    division.classList.remove("onclick-button")
+   } 
+  plus.classList.add("onclick-button")
   calcFunction()
-  display_num+="+"
   record_num = ""
   calc = "1"
-  display.textContent = display_num
+  display.textContent = calc_num
 }
 minus.onclick = function(){
   if (record_num !== ""){
+    if(plus.classList.contains("onclick-button") == true ){
+      plus.classList.remove("onclick-button")
+     } else if(multi.classList.contains("onclick-button") == true ){
+      multi.classList.remove("onclick-button")
+     } else if(division.classList.contains("onclick-button") == true ){
+      division.classList.remove("onclick-button")
+     } 
+    minus.classList.add("onclick-button")
     calcFunction()
     record_num = ""
     calc = "2"
+    display.textContent = calc_num
   } else{
     record_num+="-"
+    display.textContent = record_num
   }
-  display_num+="-"
-  display.textContent = display_num 
 }
 multi.onclick = function(){
+  if(plus.classList.contains("onclick-button") == true ){
+    plus.classList.remove("onclick-button")
+   } else if(minus.classList.contains("onclick-button") == true ){
+    minus.classList.remove("onclick-button")
+   } else if(division.classList.contains("onclick-button") == true ){
+    division.classList.remove("onclick-button")
+   } 
+  multi.classList.add("onclick-button")
   calcFunction()
-  display_num+="×"
   record_num = ""
   calc = "3"
-  display.textContent = display_num 
+  display.textContent = calc_num
 }
 division.onclick = function(){
+  if(plus.classList.contains("onclick-button") == true ){
+    plus.classList.remove("onclick-button")
+   } else if(minus.classList.contains("onclick-button") == true ){
+    minus.classList.remove("onclick-button")
+   } else if(multi.classList.contains("onclick-button") == true ){
+    multi.classList.remove("onclick-button")
+   } 
+  division.classList.add("onclick-button")
   calcFunction()
-  display_num+="÷"
   record_num = ""
   calc = "4"
-  display.textContent = display_num 
+  display.textContent = calc_num 
 }
 
 clear.onclick = function(){
-  display_num =""
   record_num = "" 
   calc_num = null
   calc = "0"
   display.textContent = 0
+  if(plus.classList.contains("onclick-button") == true ){
+    plus.classList.remove("onclick-button")
+   } else if(minus.classList.contains("onclick-button") == true ){
+    minus.classList.remove("onclick-button")
+   } else if(multi.classList.contains("onclick-button") == true ){
+    multi.classList.remove("onclick-button")
+   } else if(division.classList.contains("onclick-button") == true ){
+    division.classList.remove("onclick-button")
+}
 }
 
 dot.onclick = function(){
   record_num+="."
-  display_num+="."
-  display.textContent = display_num 
+  display.textContent = record_num 
+}
+
+plumin.onclick = function(){
+  if (record_num.indexOf("-",0)!==-1){
+    record_num = record_num.slice(1)
+    if (record_num === ""){
+      display.textContent = 0
+    } else{
+      display.textContent = record_num
+    }
+  } else{
+    record_num = "-" + record_num
+    display.textContent = record_num
+  }
 }
 
 equal.onclick = function(){ 
   calcFunction()
   display.textContent = calc_num
-  display_num =""
   record_num = "" 
   calc_num = null
   calc = "0"
+  if(plus.classList.contains("onclick-button") == true ){
+    plus.classList.remove("onclick-button")
+   } else if(minus.classList.contains("onclick-button") == true ){
+    minus.classList.remove("onclick-button")
+   } else if(multi.classList.contains("onclick-button") == true ){
+    multi.classList.remove("onclick-button")
+   } else if(division.classList.contains("onclick-button") == true ){
+    division.classList.remove("onclick-button")
 }
-
-
+}
 
